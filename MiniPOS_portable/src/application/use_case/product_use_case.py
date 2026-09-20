3f3092726626cf12fde2d90db8945a480354cb9a
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-09-20 12:52:17 -0500

    Modify product methods to include barcode

--- MiniPOS_portable/src/application/use_case/product_use_case.py
+++ MiniPOS_portable/src/application/use_case/product_use_case.py
@@ -5,31 +5,31 @@ class ProductCase:
     def __init__(self, db_manager: DBManager):
         self.db = db_manager
     
-    def add_product(self, name: str, price: float, stock: int) -> Product:
+    def add_product(self, name: str, barcode: str, price: float, stock: int) -> Product:
         conn = self.db.get_connection()
         cursor = conn.cursor()
         cursor.execute(
-            "INSERT INTO products (name, price, stock) VALUES (?,?,?)",
-            (name, price, stock)
+            "INSERT INTO products (name, barcode, price, stock) VALUES (?,?,?,?)",
+            (name, barcode, price, stock)
         )
         conn.commit()
         product_id = cursor.lastrowid
-        return Product(product_id, name, price, stock)
+        return Product(product_id, name, barcode, price, stock)
     
     def list_products(self):
         conn = self.db.get_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM products")
         rows = cursor.fetchall()
-        return [Product(row["product_id"], row["name"], row["price"], row["stock"]) for row in rows]
+        return [Product(row["product_id"], row["name"], row["barcode"], row["price"], row["stock"]) for row in rows]
 
-    def update_product(self, product_id: int, name: str, price: float, stock: int):
+    def update_product(self, product_id: int, name: str, barcode: str, price: float, stock: int):
         """Actualiza un producto existente en la base de datos"""
         conn = self.db.get_connection()
         cursor = conn.cursor()
         cursor.execute(
-            "UPDATE products SET name = ?, price = ?, stock = ? WHERE product_id = ?",
-            (name, price, stock, product_id)
+            "UPDATE products SET name = ?, barcode = ?, price = ?, stock = ? WHERE product_id = ?",
+            (name, barcode, price, stock, product_id)
         )
         conn.commit()
 
