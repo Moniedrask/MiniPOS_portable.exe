d4536bb0841eb923dedf50db141f45fecfed4fed
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-09-20 14:10:22 -0500

    Add barcode attribute to Product class

--- MiniPOS_portable/src/domain/models/product.py
+++ MiniPOS_portable/src/domain/models/product.py
@@ -1,9 +1,10 @@
 class Product:
-    def __init__(self, product_id: int, name: str, price: float, stock: int):
+    def __init__(self, product_id: int, name: str, barcode: str, price: float, stock: int):
         self.product_id = product_id
         self.name = name
+        self.barcode = barcode
         self.price = price
         self.stock = stock
 
     def __repr__(self):
-        return f"<Product {self.product_id}: {self.name} (${self.price}) Stock: {self.stock}>"
+        return f"<Product {self.product_id}: {self.name} ({self.barcode}) (${self.price}) Stock: {self.stock}>"
