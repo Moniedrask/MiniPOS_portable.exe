6d9d503149644d05b93e99e1e0db3dd9343fba13
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-09-20 21:18:52 -0500

    Actualizar main.py

--- MiniPOS_portable/main.py
+++ MiniPOS_portable/main.py
@@ -4,7 +4,7 @@ import os
 # Asegúrate de agregar 'src' al sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
-# ✅ CORRECCIÓN: Quitamos el "src." del inicio
+# ✅ Sin "src." al inicio
 from presentation.views.main_view import MainView
 
 if __name__ == "__main__":
