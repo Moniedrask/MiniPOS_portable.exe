a9f2c56e1a353515c510a728bb8bd73fb1f63f0d
Moniedrask <142689546+Moniedrask@users.noreply.github.com>
2026-09-20 20:57:55 -0500

    Actualizar product.py

--- MiniPOS_portable/src/domain/models/product.py
+++ MiniPOS_portable/src/domain/models/product.py
@@ -1,13 +1,16 @@
 class Product:
-    def __init__(self, product_id: int, name: str, barcode: str, price: float,
-                 stock: float, unit_type: str = "unidad", unit: str = "unidad"):
+    def __init__(self, product_id, name, barcode, price, stock,
+                 unit_type="unidad", unit="unidad",
+                 created_at="", updated_at=""):
         self.product_id = product_id
         self.name = name
         self.barcode = barcode
         self.price = price
         self.stock = stock
-        self.unit_type = unit_type  # "unidad" | "peso" | "volumen"
-        self.unit = unit            # "unidad" | "kg" | "gr" | "mg" | "Lt" | "ml"
+        self.unit_type = unit_type
+        self.unit = unit
+        self.created_at = created_at
+        self.updated_at = updated_at
 
     def __repr__(self):
         return f"<Product {self.product_id}: {self.name} ({self.unit}) ${self.price}>"
